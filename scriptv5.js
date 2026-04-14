6a8e800cf32a9da5a9d42a1a27a15df05072dc07
chentamil <chentamil1988@gmail.com>
2026-04-14 14:55:35 +0530

    working components, mvp-v8.64

--- scriptv5.js
+++ scriptv5.js
@@ -4,33 +4,22 @@ async function loadComponent(id, file) {
   document.getElementById(id).innerHTML = html;
 }
 
-// Favicon Injection (Patchable Section)
-async function loadFavicon() {
-  const faviconLink = document.createElement("link");
-  faviconLink.rel = "icon";
-  faviconLink.href = "https://cdn.jsdelivr.net/gh/chentamil/sdjewellers@main/images/sdj-logo.jpg";  // Your Favicon URL
-  faviconLink.type = "image/jpeg";
-  document.head.appendChild(faviconLink);
-}
-
 document.addEventListener("DOMContentLoaded", async () => {
   const base = "https://cdn.jsdelivr.net/gh/chentamil/sdjewellers@main";
-  // const base = "https://cdn.jsdelivr.net/gh/chentamil/sdjewellers@195a8b2729ab3da9fcaabe536d80fda8805382f2";
+//u can github commit number url like
+// "https://cdn.jsdelivr.net/gh/chentamil/sdjewellers@195a8b2729ab3da9fcaabe536d80fda8805382f2";
 
-  // 1. Load Favicon
-  await loadFavicon();
-
-  // 2. Load header & footer
+  // 1. Load header & footer
   await loadComponent("header", base + "/components/header.html");
   await loadComponent("footer", base + "/components/footer.html");
 
-  // 3. Load Bootstrap (ONLY ONCE)
+  // 2. Load Bootstrap (ONLY ONCE)
   await loadScript("https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js");
 
-  // 4. Load rate.js properly
+  // 3. Load rate.js properly
   await loadScript(base + "/js/ratev1.js");
 
-  // 5. Call function AFTER everything is ready
+  // 4. Call function AFTER everything is ready
   if (typeof loadRates === "function") {
     loadRates();
   } else {
@@ -38,7 +27,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
 });
 
-// Helper to load external scripts
+// helper function to load external scripts
 function loadScript(src) {
   return new Promise((resolve, reject) => {
     const s = document.createElement("script");
